eeb21903f869d09792638b2ca5d4903d5a174240
Ari Fordsham <arifordsham@gmail.com>
2023-07-09 17:32:41 +0300

    Added nixosSystem

--- flake.nix
+++ flake.nix
@@ -1,5 +1,8 @@
 {
-  outputs = {...}: {
-    nixosConfigurations."nixos" = {};
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+  };
+  outputs = {nixpkgs, ...}: {
+    nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {};
   };
 } 
