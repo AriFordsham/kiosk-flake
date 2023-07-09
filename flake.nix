218a5a1e5c2e8a40878eabe1458d67ef584935b6
Ari Fordsham <arifordsham@gmail.com>
2023-07-09 17:39:14 +0300

    added virtualbox modules

--- flake.nix
+++ flake.nix
@@ -3,6 +3,11 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
   };
   outputs = {nixpkgs, ...}: {
-    nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {};
+    nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        nixpkgs + "/nixos/modules/installer/virtualbox-demo.nix"
+      ];
+    };
   };
 } 
