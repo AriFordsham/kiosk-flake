371e2c6a75f4ff4bfe0b7d38fb679e1d71fcb808
Ari Fordsham <arifordsham@gmail.com>
2023-07-11 16:37:33 +0300

    setup users and ssh

--- kiosk.nix
+++ kiosk.nix
@@ -1,15 +1,32 @@
-{pkgs, ...}:{
-    hardware.opengl.enable = true;
+{ pkgs, ... }: {
+  hardware.opengl.enable = true;
 
-    users.mutableUsers = false;
+  users.mutableUsers = false;
 
-    users.users.kiosk = {
-      isNormalUser = true;
+  users.users.admin = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    password = "demo";
+    uid = 1000;
+    openssh.authorizedKeys.keys = [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC7r1762nBE9yTHQNv9FhyQHj3YFiu+gzO92Fr8owqsKET6nC7sOGvpOu3uCjf5HuY6Xm7InBTODt07efAD1JdsX3INAw/5RA+csTdTcYuAaff5CzL8IhjXWYv1JfXCaORqyZgIoyDxxD2bfbgTWosqgt3kfmEFovuyMCny+TwpaP65vG2nI6NTPYxKUlEYykJ6YwHI9LwVjOSSFWLv+xNlux4FlmUS8WU1V6trAvB/nIw+QeqjJsn34CKSUrQ2GOvFIRUkExnjhKQaYqW78taU7qKEEMbVfEvC9d5//veVIP8nVpSdJ1u9KRsI6DBUsDAW8Kp3luIdRVFcoRHOyYp+lV9G8zgdc8KqjZBvWWxW9xd05rMQyBO4XWjbNfF84NSlBxAcBy8dNW9in7D7xn3G+O1fa0hVe+DBiEaEt5jOWSQYJh1ahz/cwUjvosjTVERFOPtsABgn+4N2pt5FRdoxxv1V9rzshiee+GIxP4AciLxWAUxa7rKyaUJ4aI+e730= ari"
+    ];
+  };
+
+  services.openssh = {
+    enable = true;
+    settings = {
+      PermitRootLogin = "no";
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
     };
+  };
 
-    services.cage = {
-      enable = true;
-      user = "kiosk";
-      program = "${pkgs.chromium}/bin/chromium";
-    };
+  users.users.kiosk = { isNormalUser = true; };
+
+  services.cage = {
+    enable = true;
+    user = "kiosk";
+    program = "${pkgs.chromium}/bin/chromium";
+  };
 }
--- virtualbox-demo.nix
+++ virtualbox-demo.nix
@@ -1,19 +1,9 @@
 { lib, pkgs, nixpkgs, ... }: {
   imports = [ (nixpkgs + /nixos/modules/virtualisation/virtualbox-image.nix) ];
 
-  users.users.demo = {
-    isNormalUser = true;
-    description = "Demo user account";
-    extraGroups = [ "wheel" ];
-    password = "demo";
-    uid = 1000;
-  };
-
   # FIXME: UUID detection is currently broken
   boot.loader.grub.fsIdentifier = "provided";
 
   powerManagement.enable = false;
   system.stateVersion = lib.mkDefault "18.03";
-
-  users.users.root.password = "12345";
 }
