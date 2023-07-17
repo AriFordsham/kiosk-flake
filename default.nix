940cdb4a664fcc915eb5ad9d2eca4cafe1265962
Ari Fordsham <arifordsham@gmail.com>
2023-07-17 16:49:54 +0300

    Setup default/nix properly

--- default.nix
+++ default.nix
@@ -1,1 +1,11 @@
-{}
+{
+  imports = [
+    <nixpkgs/nixos/modules/virtualisation/hyperv-guest.nix>
+    /etc/nixos/hardware-configuration.nix
+    ./netfree.nix
+    ./kiosk.nix
+  ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+}
