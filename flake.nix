b18b6db73c187fbbb8f797dd561a0247b5405850
Ari Fordsham <arifordsham@gmail.com>
2023-07-17 16:30:40 +0300

    Disable grub

--- flake.nix
+++ flake.nix
@@ -8,6 +8,7 @@
       modules = [
         (nixpkgs + /nixos/modules/virtualisation/hyperv-guest.nix)
         {
+          boot.loader.grub.enable = false;
           boot.loader.systemd-boot.enable = true;
           boot.loader.efi.canTouchEfiVariables = true;
         }
